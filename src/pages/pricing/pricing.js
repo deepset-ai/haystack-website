194874c0a8b4648726e389d27f37d8520fca6f3c
PiffPaffM <markuspaff.mp@gmail.com>
2020-10-06 16:55:38 +0200

    add update for benchmarks and new pricing

--- src/pages/pricing/pricing.js
+++ src/pages/pricing/pricing.js
@@ -3,55 +3,10 @@ import Layout from '../../components/layout/layout';
 
 import './pricing.scss';
 
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-//import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
 import Products from "../../components/landing-page/products";
 import Button from "../../components/landing-page/button";
 
 const PricingPage = () => {
-
-    const tiers = [
-        {
-          title: 'Neural Search',
-          subheader: 'Starter',
-          price: '500€ per month',
-          description: [
-            '2,000 pages or 10 Documents',
-            '100 requests per day',
-          ],
-          buttonText: 'Free Trial',
-          buttonVariant: 'contained',
-        },
-        {
-          title: 'Neural Search',
-          subheader: 'Basic',
-          price: '1000€ per month',
-          description: [
-            '20,000 pages or 100 Documents',
-            '1000 requests per day',
-          ],
-          buttonText: 'Free Trial',
-          buttonVariant: 'contained',
-        },
-        {
-          title: 'Neural Search',
-          subheader: 'Premium',
-          price: '5000€ per month',
-          description: [
-            '400,000 pages or 2000 Documents',
-            '5000 requests per day',
-          ],
-          buttonText: 'Free Trial',
-          buttonVariant: 'contained',
-        }
-      ];
 
   return (
       <Layout>
@@ -59,42 +14,28 @@ const PricingPage = () => {
         <Products />
         </div>
         <div className="pricing">
-        <h2>Haystack Hub Pricing</h2>
-        <Container component="main" maxWidth="xl">
-            <Grid container spacing={5}>
-              {tiers.map(tier => (
-                // Enterprise card is full width at sm breakpoint
-                <Grid item key={tier.title} md={4}>
-                    <Card className="card-item">
-                      <CardHeader
-                        title={tier.title}
-                        subheader={tier.subheader}
-                        titleTypographyProps={{ align: 'center' }}
-                        subheaderTypographyProps={{ align: 'center' }}
-                        className="cardHeader"
-                      />
-                      <CardContent>
-                        <div className="cardPricing">
-                          <Typography component="h2" variant="h3" color="textPrimary">
-                            {tier.price}
-                          </Typography>
-                        </div>
-                        <ul>
-                          {tier.description.map(line => (
-                            <Typography component="li" variant="subtitle1" align="center" key={line}>
-                              {line}
-                            </Typography>
-                          ))}
-                        </ul>
-                      </CardContent>
-                      <CardActions className="card-action">
-                        <Button className="item-button" label={tier.buttonText} to={tier.to}/>
-                      </CardActions>
-                    </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
+          <div className="pricing-options pricing-starter">
+            <h4>Neural Search</h4>
+            <div className="img-starter">Image</div>
+            <h3 className="h3-starter">Starter</h3>
+            <div>&euro; <span>500</span> /month</div>
+            <div>2,000 pags or 10 Documents<br/>10 requests per day</div>
+          </div>
+          <div className="pricing-options pricing-basic">
+            <h4>Neural Search</h4>
+            <div className="img-basic">Image</div>
+            <h3 className="h3-basic">Basic</h3>
+            <div>&euro; <span>1000</span> /month</div>
+            <div>20,000 pags or 100 Documents<br/>1000 requests per day</div>
+          </div>
+          <div className="pricing-options pricing-premium">
+            <h4>Neural Search</h4>
+            <div className="img-premium">Image</div>
+            <h3 className="h3-premium">Premium</h3>
+            <div>&euro; <span>5000</span> /month</div>
+            <div>40,000 pags or 2000 Documents<br/>5000 requests per day</div>
+          </div>
+          </div>
           <div className="product-enterprise">
             <div>
               <h2>Neural Search Enterprise</h2>
@@ -102,7 +43,7 @@ const PricingPage = () => {
             </div>
             <Button className="item-button" label="Contanct Us" />
           </div>
-          </div>
+
       </Layout>
   );
 
