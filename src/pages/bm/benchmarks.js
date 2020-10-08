97a1e4981f888ea73f17b942ab8ea650e458d01f
PiffPaffM <markuspaff.mp@gmail.com>
2020-10-08 18:28:46 +0200

    new line charts

--- src/pages/bm/benchmarks.js
+++ src/pages/bm/benchmarks.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react"
 import JSONDataReader from "../benchmarks/site/en/reader_performance.json"
 import JSONDataRetriever from "../benchmarks/site/en/retriever_performance.json"
-import JSONLineChart1 from "../benchmarks/site/en/lineChart1.json"
-import JSONLineChart2 from "../benchmarks/site/en/lineChart2.json"
+import JSONRetrieverMap from "../benchmarks/site/en/retriever_map.json"
+import JSONRetrieverSpeed from "../benchmarks/site/en/retriever_speed.json"
 import Layout from "../../components/layout/layout";
 
 import { Chart } from "react-google-charts";
@@ -22,13 +22,24 @@ const BenchMarks = () => {
 
     let dataReader = new Array(JSONDataReader.columns);
     for (let i=0; i<JSONDataReader.data.length; i++) {
-        dataReader[i+1] = [JSONDataReader.data[i].model, JSONDataReader.data[i].accuracy, JSONDataReader.data[i].time];
+        dataReader[i+1] = [JSONDataReader.data[i].Model, JSONDataReader.data[i].F1, JSONDataReader.data[i].Speed];
     } 
 
     let dataRetriever = new Array(JSONDataRetriever.columns);
     for (let i=0; i<JSONDataRetriever.data.length; i++) {
-        dataRetriever[i+1] = [JSONDataRetriever.data[i].model, JSONDataRetriever.data[i].recall, JSONDataRetriever.data[i].index_time, JSONDataRetriever.data[i].query_time];
+        dataRetriever[i+1] = [JSONDataRetriever.data[i].model, JSONDataRetriever.data[i].n_docs, JSONDataRetriever.data[i].index_speed, JSONDataRetriever.data[i].query_speed, JSONDataRetriever.data[i].map];
     } 
+
+    let dataRetrieverSpeed = new Array(JSONRetrieverSpeed.columns);
+    for (let i=0; i<JSONRetrieverSpeed.data.length; i++) {
+      dataRetrieverSpeed[i+1] = JSONRetrieverSpeed.data[i];
+    }
+    
+    let dataRetrieverMap = new Array(JSONRetrieverMap.columns);
+    for (let i=0; i<JSONRetrieverMap.data.length; i++) {
+      dataRetrieverMap[i+1] = JSONRetrieverMap.data[i];
+    }
+    
 
     useEffect(()=>  {
       window.addEventListener("click", () => {
@@ -63,6 +74,7 @@ const BenchMarks = () => {
                 bars: JSONDataReader.bars
                 }}
             />
+
             <button className="h-button" onKeyDown={() => {setShowRetrieverDesc(!showRetrieverDesc);}}
                     onClick={(e) => { e.stopPropagation(); setShowRetrieverDesc(!showRetrieverDesc);}}>
             <h2>{JSONDataRetriever.title} {showRetrieverDesc ? (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronUp}/>) : (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronDown}/>)}</h2>
@@ -70,7 +82,6 @@ const BenchMarks = () => {
             {showRetrieverDesc && (
               <div className="desc-details">{JSONDataRetriever.description}</div>
             )}
-
             <Chart
                 width={1200}
                 height={600}
@@ -97,38 +108,38 @@ const BenchMarks = () => {
 
             <button className="h-button" onKeyDown={() => {setLineChart1Desc(!showLineChart1Desc);}}
                     onClick={(e) => { e.stopPropagation(); setLineChart1Desc(!showLineChart1Desc);}}>
-            <h2>{JSONLineChart1.title} {showLineChart1Desc ? (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronUp}/>) : (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronDown}/>)}</h2>
+            <h2>{JSONRetrieverMap.title} {showLineChart1Desc ? (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronUp}/>) : (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronDown}/>)}</h2>
             </button>
             {showLineChart1Desc && (
-              <div className="desc-details">{JSONLineChart1.description}</div>
+              <div className="desc-details">{JSONRetrieverMap.description}</div>
             )}
             <Chart
                 width={1200}
                 height={600}
-                chartType={JSONLineChart1.chart_type}
+                chartType={JSONRetrieverMap.chart_type}
                 loader={<div>Loading Chart</div>}
-                data={JSONLineChart1.data}
+                data={dataRetrieverMap}
                 options={{
-                    subtitle: JSONLineChart1.subtitle,
+                    subtitle: JSONRetrieverMap.subtitle,
                     colors: ['#22BA99', '#63C7CA', '#49B0E4', '#FBB14B'],
                 }}
             />
 
             <button className="h-button" onKeyDown={() => {setLineChart2Desc(!showLineChart2Desc);}}
                     onClick={(e) => { e.stopPropagation(); setLineChart2Desc(!showLineChart2Desc);}}>
-            <h2>{JSONLineChart2.title} {showLineChart2Desc ? (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronUp}/>) : (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronDown}/>)}</h2>
+            <h2>{JSONRetrieverSpeed.title} {showLineChart2Desc ? (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronUp}/>) : (<FontAwesomeIcon class="fontawsome-icon" icon={faChevronDown}/>)}</h2>
             </button>
             {showLineChart2Desc && (
-              <div className="desc-details">{JSONLineChart2.description}</div>
+              <div className="desc-details">{JSONRetrieverSpeed.description}</div>
             )}
             <Chart
                 width={1200}
                 height={600}
-                chartType={JSONLineChart2.chart_type}
+                chartType={JSONRetrieverSpeed.chart_type}
                 loader={<div>Loading Chart</div>}
-                data={JSONLineChart2.data}
+                data={dataRetrieverSpeed}
                 options={{
-                    subtitle: JSONLineChart2.subtitle,
+                    subtitle: JSONRetrieverSpeed.subtitle,
                     colors: ['#22BA99', '#63C7CA', '#49B0E4', '#FBB14B'],
                 }}
             />
