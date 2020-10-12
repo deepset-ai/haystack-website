68bcb214e5682721b49026342661247b114f5bfc
Markus Paff <markuspaff.mp@gmail.com>
2020-10-12 20:42:27 +0200

    Add benchmarking (#28)
    * demo page benchmarking
    
    * add description toggle for benchmarking
    
    * add update bechmarking numbers
    
    * added line charts
    
    * new line charts
    
    * added benchmarks
    
    * added axis and point
    
    * design finte tuning
    
    * design fine tuning
    
    * add italic
    
    * add thumbnail
    
    * added seo
    
    * add seo for linkedin
    
    * social card optimization

--- src/components/seo.js
+++ src/components/seo.js
@@ -79,7 +79,7 @@ function SEO({ description, lang, meta, image: metaImage, title, pathname  }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
