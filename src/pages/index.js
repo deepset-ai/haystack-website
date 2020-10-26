88b2657817b859959b856b872df4bd0c68e0597c
PiffPaffM <markuspaff.mp@gmail.com>
2020-10-26 01:55:46 +0100

    fixed widht mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,10 @@ import Layout from "../components/layout/layout";
 import SEO from "../components/seo";
 import "react-github-button/assets/style.css";
 import "../scss/index.scss";
-//import IconJPG from "../images/HaystackIcon.jpg"
 import Logo from "../images/haystack_logo_blue_banner_social.png"
 
 import ValueProp from "../components/landing-page/valueProp";
 import ProductFeatures from "../components/landing-page/productFeatures";
-//import UserProof from "../components/landing-page/userProof";
 import Products from "../components/landing-page/products";
 import UseCases from "../components/landing-page/useCases";
 import CallToAction from "../components/landing-page/callToAction";
