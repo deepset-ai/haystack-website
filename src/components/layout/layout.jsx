39a8239b8ae7bdbc566cf059a80698bde878c8ef
PiffPaffM <markuspaff.mp@gmail.com>
2020-11-18 08:53:56 +0100

    cookies message

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -9,6 +9,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Header from "../header/header";
 import Footer from "../footer/footer";
+import CookieConsent from "react-cookie-consent";
 
 import "./layout.scss";
 import "../../reset.scss";
@@ -17,6 +18,17 @@ const Layout = ({ children, language="en", locale="en" }) => {
     <>
       <Header language={language} locale={locale} />
       <div className="content-wrapper">{children}</div>
+      <CookieConsent
+        location="bottom"
+        buttonText="OK, DON'T SHOW AGAIN"
+        style={{ background: "#08415D", width: "40%", "text-align": "center", 
+          "border-radius": "8px", "margin-left": "30%", "margin-bottom": "10px", }}
+        buttonWrapperClasses="cookies-button-wrapper"
+        buttonStyle={{ background: "#009BAA", fontSize: "13px", color: "#ffffff", "border-radius": "4px"}}
+        expires={150}
+      >
+        We use cookies to provide the best site experience.
+      </CookieConsent>
       <Footer language={language} locale={locale}></Footer>
     </>
   );
