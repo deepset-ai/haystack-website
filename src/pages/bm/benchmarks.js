67ad7c39349ea0e9bd3b89c33e68b288ffa6e187
brandenchan <brandenchan@icloud.com>
2020-11-19 12:08:25 +0100

    Change color of other graph

--- src/pages/bm/benchmarks.js
+++ src/pages/bm/benchmarks.js
@@ -230,7 +230,7 @@ const BenchMarks = ({data}) => {
                 loader={<div>Loading Chart</div>}
                 data={mapDataReader[selectedVersion]}
                 options={{
-                colors: ['#22BA99', '#FBB14B', '#49B0E4'],
+                colors: ['#22BA99', '#63C7CA', '#49B0E4', '#FBB14B'],
                 subTitle: subTitelReader[selectedVersion],
                 bars: barsReader[selectedVersion],
                 legend: "bottom"
@@ -284,7 +284,7 @@ const BenchMarks = ({data}) => {
                 data={mapDataMap[selectedVersion]}
                 options={{
                     subtitle: subTitleMap[selectedVersion],
-                    colors: ['#22BA99', '#63C7CA', '#49B0E4', '#FBB14B'],
+                    colors: ['#22BA99', '#FBB14B', '#63C7CA', '#49B0E4'],
                     hAxis: {
                       title: axisXMap[selectedVersion]
                     },
