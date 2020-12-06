65c4c2e2546cedeb8b0fc6fa459a439802c4e285
PiffPaffM <markuspaff.mp@gmail.com>
2020-12-06 08:11:27 +0100

    npm run function

--- broken-link-checker.sh
+++ broken-link-checker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-start=$SECONDS
-
 urls=$(curl http://haystackhub-website.s3-website.eu-central-1.amazonaws.com/sitemap.xml | sitemap-urls)
 
 for url in $(echo $urls | tr " " "\n")
@@ -9,7 +7,5 @@ do
     url="${url/https:\/\/haystack.deepset.ai/http:\/\/haystackhub-website.s3-website.eu-central-1.amazonaws.com}"
     blc $url -ro --exclude https://www.linkedin.com/company/deepset-ai/ --exclude http://127.0.0.1:8000/docs --firlter-level 1
 done
-
-duration=$(( SECONDS - start ))
 
 echo $duration
