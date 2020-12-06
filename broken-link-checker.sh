716e54393396a4fcd3435a348e7523699b5da143
PiffPaffM <markuspaff.mp@gmail.com>
2020-12-06 08:25:57 +0100

    fix runtime error

--- broken-link-checker.sh
+++ broken-link-checker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-urls=$(curl http://haystackhub-website.s3-website.eu-central-1.amazonaws.com/sitemap.xml | sitemap-urls)
+urls=$(curl http://haystackhub-website.s3-website.eu-central-1.amazonaws.com/sitemap.xml  | tac | tac | sitemap-urls)
 
 for url in $(echo $urls | tr " " "\n")
 do
-    url="${url/https:\/\/haystack.deepset.ai/http:\/\/haystackhub-website.s3-website.eu-central-1.amazonaws.com}"
+    if [ "$1" = "dev" ]; then
+        url="${url/https:\/\/haystack.deepset.ai/http:\/\/haystackhub-website.s3-website.eu-central-1.amazonaws.com}"
+    fi
     blc $url -ro --exclude https://www.linkedin.com/company/deepset-ai/ --exclude http://127.0.0.1:8000/docs --firlter-level 1
 done
 
