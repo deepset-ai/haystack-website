e1e34c0d89c93ec1f0bdbb4a4a89fb9d35bfe003
PiffPaffM <markuspaff.mp@gmail.com>
2020-12-07 15:05:02 +0100

    optimize link checker and fix links

--- broken-link-checker.sh
+++ broken-link-checker.sh
@@ -11,5 +11,13 @@ do
     if [ "$1" = "dev" ]; then
         url="${url/https:\/\/haystack.deepset.ai/http:\/\/haystackhub-website.s3-website.eu-central-1.amazonaws.com}"
     fi
-    blc $url -ro --exclude https://www.linkedin.com/company/deepset-ai/ --exclude http://127.0.0.1:8000/docs --firlter-level 1
+    if [ $url == *"v0.4.0"* ] || [ $url == *"v0.5.0"* ]; then
+        continue
+    fi
+    blc $url -ro --exclude https://www.linkedin.com/company/deepset-ai/ --exclude http://127.0.0.1:8000/docs --firlter-level 1 --host-requests 4
+    status=$?
+    
+    if [ "$status" != "0" ]; then
+        exit 1
+    fi
 done
