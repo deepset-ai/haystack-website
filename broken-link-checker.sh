6a6ef31b9662a08a0e690984389f9a2a9440bc6b
PiffPaffM <markuspaff.mp@gmail.com>
2020-12-10 20:01:54 +0100

    test openapi

--- broken-link-checker.sh
+++ broken-link-checker.sh
@@ -18,12 +18,14 @@ do
     --exclude http://127.0.0.1:8000/docs \
     --exclude https://medium.com/deepset-ai/modern-question-answering-systems-explained-4d0913744097 \
     --exclude http://haystackhub-website.s3-website.eu-central-1.amazonaws.com/docs/v0.4.0/tutorial2md \
+    --exclude https://haystack.deepset.ai/docs/v0.4.0/tutorial2md \
     --exclude https://raw.githubusercontent.com/deepset-ai/haystack/master/docs/img/annotation_tool.png \
     --exclude http://haystackhub-website.s3-website.eu-central-1.amazonaws.com/docs/v0.4.0/tutorial6md \
+    --exclude https://haystack.deepset.ai/docs/v0.4.0/tutorial6md \
     --exclude https://raw.githubusercontent.com/deepset-ai/haystack/master/docs/img/colab_gpu_runtime.jpg \
     --exclude http://github.com/deepset-ai/haystack/ \
     --exclude http://github.com/deepset-ai/haystack/stargazers/ \
-    --firlter-level 1 --host-requests 4
+    --firlter-level 1 --host-requests 1
     status=$?
     
     if [ "$status" != "0" ]; then
