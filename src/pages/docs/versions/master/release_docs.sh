542e2bd3fcbceb356b010cb256b57ca2267c5fb9
Markus Paff <markuspaff.mp@gmail.com>
2021-04-13 17:43:37 +0200

    new docs version

--- src/pages/docs/versions/master/release_docs.sh
+++ src/pages/docs/versions/master/release_docs.sh
@@ -10,10 +10,10 @@ echo " " >> versions.txt
 # Remove empty lines
 tr -d '\n' < versions.txt > temp.txt && mv temp.txt versions.txt
 
-npm run update_apidocs_dev $3
-npm run update_tutorialsdocs_dev $3
-npm run update_usagedocs_dev $3
-npm run update_img_dev $3
-npm run update_benchmarks_dev $3
+npm run update_apidocs_dev $2
+npm run update_tutorialsdocs_dev $2
+npm run update_usagedocs_dev $2
+npm run update_img_dev $2
+npm run update_benchmarks_dev $2
 
 cp -r site/en/menuStructure/ $1site/en/
