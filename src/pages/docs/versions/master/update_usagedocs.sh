9dbba99f43618897bafde63115d120324bb9c2f9
brandenchan <brandenchan@icloud.com>
2021-06-10 17:18:07 +0200

    Add faq page

--- src/pages/docs/versions/master/update_usagedocs.sh
+++ src/pages/docs/versions/master/update_usagedocs.sh
@@ -71,6 +71,9 @@ do
     if [[ `wget -S --spider https://raw.githubusercontent.com/deepset-ai/haystack/${1}/docs/${j}_src/usage/usage/ranker.md 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
         wget https://raw.githubusercontent.com/deepset-ai/haystack/${1}/docs/${j}_src/usage/usage/ranker.md  -O ./src/pages/docs/versions/master/${i}site/en/usage/usage/ranker.md
     fi
+    if [[ `wget -S --spider https://raw.githubusercontent.com/deepset-ai/haystack/${1}/docs/${j}_src/usage/usage/faq.md 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+        wget https://raw.githubusercontent.com/deepset-ai/haystack/${1}/docs/${j}_src/usage/usage/faq.md  -O ./src/pages/docs/versions/master/${i}site/en/usage/usage/faq.md
+    fi
 
     sed -i 's/<!---/---/' ./src/pages/docs/versions/master/${i}site/en/usage/usage/roadmap.md
     sed -i 's/--->/---/' ./src/pages/docs/versions/master/${i}site/en/usage/usage/roadmap.md 
@@ -139,4 +142,7 @@ do
         sed -i 's/<!---/---/' ./src/pages/docs/versions/master/${i}site/en/usage/usage/ranker.md
         sed -i 's/--->/---/' ./src/pages/docs/versions/master/${i}site/en/usage/usage/ranker.md
     fi
-done
+    if [[ -f "./src/pages/docs/versions/master/${i}site/en/usage/usage/faq.md" ]]; then
+        sed -i 's/<!---/---/' ./src/pages/docs/versions/master/${i}site/en/usage/usage/faq.md
+        sed -i 's/--->/---/' ./src/pages/docs/versions/master/${i}site/en/usage/usage/faq.md
+    fi
