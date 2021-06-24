f3338a3aceec2b16b282130809c30c8f8fb74974
PiffPaffM <markuspaff.mp@gmail.com>
2021-06-24 18:37:54 +0200

    ignore a link issue for testing

--- broken-link-checker.sh
+++ broken-link-checker.sh
@@ -31,6 +31,7 @@ do
     --exclude https://deepset.ai/german-bert \
     --exclude https://deepset.ai/germanquad \
     --exclude https://ext-models-haystack.s3.eu-central-1.amazonaws.com/ \
+    --exclude https://github.com/deepset-ai/haystack/blob/master/rest_api/pipelines.yaml \
     --filter-level 1 --host-requests 1 
     status=$?
     
