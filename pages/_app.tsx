724b860e9f030761e13a6b6d38580a5eedb30bf3
PiffPaffM <markuspaff.mp@gmail.com>
2021-09-10 11:50:42 +0200

    remove google router id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,7 @@
 import "../styles/global.css";
 import "../styles/prism-theme.css";
 import type { AppProps } from "next/app";
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
-declare const window: any;
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      });
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
 
   return <Component {...pageProps} />;
 }
