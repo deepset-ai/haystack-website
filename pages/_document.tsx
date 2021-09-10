f4ebe260b07f70f09ebb14ac7b7678997ecbc5c3
Markus Paff <markuspaff.mp@gmail.com>
2021-09-10 11:26:30 +0200

    change to google tag manager (#145)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,24 +16,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })
+            (window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_ANALYTICS}');
           `,
             }}
           />
         </Head>
         <body>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_ANALYTICS}"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
