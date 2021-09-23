d2689386c70d6d7a9bc65fbb156807ed29454547
Markus Paff <markuspaff.mp@gmail.com>
2021-09-23 16:32:14 +0200

    New docs version (#159)
    * new docs version
    
    * add version manually for testing
    
    * add more information to read.me
    
    * documentstores as dropdown
    
    * dorpdown for pros and cons documentstore
    
    * fix link
    
    * missing )
    
    * point again to haystack master

--- lib/github.ts
+++ lib/github.ts
@@ -17,7 +17,7 @@ export const getDownloadUrl = async ({
     const res = await octokit.rest.repos.getContent({
       owner: "deepset-ai",
       repo: "haystack",
-      path: `docs${version && version !== "latest" ? `/${version}` : ""}${repoPath}${filename}`,
+      path: `docs${version && version !== "latest" ? `/${version}` : ""}${repoPath}${filename}`
     });
     if (Array.isArray(res.data)) return;
     if (!res.data.download_url) return;
