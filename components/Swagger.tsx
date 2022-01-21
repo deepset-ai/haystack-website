174526ed1aca01d7dc5c18a3b7baea71c2acef89
ZanSara <sarazanzo94@gmail.com>
2022-01-21 16:15:51 +0100

    Remove the test openapi.json to fetch the actual one from Haystack's github repo

--- components/Swagger.tsx
+++ components/Swagger.tsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-import SwaggerUi, {presets} from 'swagger-ui';
+import SwaggerUi from 'swagger-ui';
 import 'swagger-ui/dist/swagger-ui.css';
 
 class Swagger extends Component {
   componentDidMount() {
     SwaggerUi({
       dom_id: '#swaggerContainer',
-      url: `/openapi/openapi.json`,
-      presets: [presets.apis],
+      // Temporary refers to a branch, update it right before merging https://github.com/deepset-ai/haystack/pull/2047
+      url: `https://raw.githubusercontent.com/deepset-ai/haystack/add_api_desc_for_rest_api/docs/_src/api/openapi/openapi.json`,
+      // Disables the Try Out buttons on the UI
       supportedSubmitMethods: [],
     });
   }
