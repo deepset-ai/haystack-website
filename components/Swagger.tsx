955c58f8bd6ab0f52eca06720f05d5d3311a38aa
Sara Zan <sarazanzo94@gmail.com>
2022-02-16 12:00:21 +0100

    Update URL for openapi.json

--- components/Swagger.tsx
+++ components/Swagger.tsx
@@ -8,7 +8,7 @@ class Swagger extends Component {
       dom_id: '#swaggerContainer',
       
       // Temporary refers to a branch, update it right before merging https://github.com/deepset-ai/haystack/pull/2047
-      url: `https://raw.githubusercontent.com/deepset-ai/haystack/add_api_desc_for_rest_api/docs/_src/api/openapi/openapi.json`,
+      url: `https://raw.githubusercontent.com/deepset-ai/haystack/master/docs/_src/api/openapi/openapi.json`,
       
       // Disables the Try Out buttons on the UI
       supportedSubmitMethods: [],
