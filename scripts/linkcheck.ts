2455376d6efa271dbf5df2e27171b06a94818135
Massimiliano Pippi <mpippi@gmail.com>
2022-05-20 15:15:38 +0200

    crawl all the available versions

--- scripts/linkcheck.ts
+++ scripts/linkcheck.ts
@@ -1,22 +1,38 @@
 import { green, red } from "chalk";
 import { exec } from "child_process";
 import * as fs from "fs";
+import { getHaystackReleaseTagNames } from "../lib/github";
 
-const logFile: string = "linkcheck.log";
-const localUrl: string = "http://localhost:3000";
-const excludeDomains: string = "googletagmanager.com";
-const cmd: string = `wget --spider -r -nd -nv -H -l 1 --exclude-domains ${excludeDomains} -o ${logFile}  ${localUrl}`;
+async function main() {
+  const logFile: string = "linkcheck.log";
+  const excludeDomains: string = "googletagmanager.com";
 
-console.log(`Crawling ${localUrl} recursively...`);
+  var versions = await getHaystackReleaseTagNames();
+  versions = versions.filter((tagName) => tagName.startsWith("v"));
+  versions = versions.slice(0, 5);
+  // prepend `latest` version, i.e. no version string in the URL
+  versions = [""].concat(versions);
+  console.log(versions);
 
-exec(cmd, (error, stdout, stderr) => {
-  const crawlingLogs: string = fs.readFileSync(logFile, "utf8");
-  const idx: number = crawlingLogs.search(/Found \d+ broken link[s]?\./g);
-  if (idx != -1) {
-    console.log(red("error ") + crawlingLogs.substring(idx));
-    process.exitCode = 1;
-    return;
+  for (var version of versions) {
+    const localUrl: string = `http://localhost:3000/overview/${version}/intro`;
+    const cmd: string = `wget --spider -r -nd -nv -H -l 1 --exclude-domains ${excludeDomains} -o ${logFile}  ${localUrl}`;
+
+    console.log(`Crawling ${localUrl} recursively...`);
+    exec(cmd, (error, stdout, stderr) => {
+      const crawlingLogs: string = fs.readFileSync(logFile, "utf8");
+      const idx: number = crawlingLogs.search(/Found \d+ broken link[s]?\./g);
+      if (idx != -1) {
+        console.log(
+          red(`error checking ${localUrl}: `) + crawlingLogs.substring(idx)
+        );
+        process.exitCode = 1;
+        return;
+      }
+
+      console.log(green("success ") + `No broken links found in ${localUrl}`);
+    });
   }
+}
 
-  console.log(green("success ") + "No broken links found.");
-});
+main();
