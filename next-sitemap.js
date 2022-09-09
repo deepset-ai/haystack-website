8cdfd28636be183b44b93b2a39e0ae68c128f306
Tuana Celik <tuana.celik@deepset.ai>
2022-09-09 18:23:40 +0300

    new sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://haystack.deepset.ai',
     generateRobotsTxt: true, 
+    exclude: ['/tutorials/v*','/overview/v*', '/components/v*', '/usage/*', '/reference/v*', '/guides/v*', '/pipeline_nodes/v*', '/benchmarks/v*']
   }
