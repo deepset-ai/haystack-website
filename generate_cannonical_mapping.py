b373c527e35eba7c1c103b82cb41fd0167965734
brandenchan <brandenchan@icloud.com>
2022-09-15 12:09:33 +0200

    Refine canonical mapping script

--- generate_cannonical_mapping.py
+++ generate_cannonical_mapping.py
@@ -113,8 +113,6 @@ def match(readme_row, df_readme):
     filenames = [
         base_filename,
         base_filename + "-api",
-        base_filename + "_api_reference",
-        base_filename + "-1",
         base_filename.replace("_definition", ""),
         base_filename.replace("_", "-") + "-api",
         base_filename.replace("_", "-")
@@ -130,53 +128,106 @@ def match(readme_row, df_readme):
         return None
     return haystack_match["filepath"].values[0]
 
+def rename_files(f):
+    new_f = f
+    new_f = new_f.replace("file-classifier-api-1", "file-converter-api")
+    new_f = new_f.replace("_", "-")
+    new_f = new_f.replace("-reference", "")
+    new_f = new_f.replace("-1", "-api")
+    new_f = new_f.replace("https://haystack.readme.io/", "https://docs.haystack.deepset.ai/")
+    return new_f
 
-readme_files = get_readme_files()
-readme_filenames = [x.split("/")[-1] for x in readme_files]
-readme_files_versions = [extract_version(x) for x in readme_files]
-readme_files_type = [extract_type(x) for x in readme_files]
-readme = zip(readme_files, readme_filenames, readme_files_versions, readme_files_type)
-df_readme = pd.DataFrame.from_records(readme, columns=["filepath", "filename", "version", "type"])
-df_readme = filter_rows_by_values(
-    df_readme,
-    "version",
-    OLD_VERSIONS
-)
-print()
+def update_readme_filenames(readme):
+    ret = []
+    for filepath, readme_filename, v, type in readme:
+        new_filename = readme_filename
+        new_filepath = filepath
+        if type == "reference":
+            new_filename = rename_files(readme_filename)
+            new_filepath = rename_files(filepath)
+        ret.append((new_filepath, new_filename, v, type))
+    return ret
 
-haystack_docs = get_all_files("docs")
-haystack_docs_type = ["docs"] * len(haystack_docs)
-haystack_api = get_all_api_files("../haystack/docs")
-haystack_api_type = ["reference"] * len(haystack_api)
-haystack_files = haystack_docs + haystack_api
-haystack_filepath = [x[2] for x in haystack_files]
-haystack_filename = [x[1] for x in haystack_files]
-haystack_types = haystack_docs_type + haystack_api_type
-haystack_versions = [extract_version(x[0]) for x in haystack_files]
-haystack = zip(haystack_filepath, haystack_filename, haystack_versions, haystack_types)
-df_haystack = pd.DataFrame.from_records(haystack, columns=["filepath", "filename", "version", "type"])
-df_haystack = filter_rows_by_values(
-    df_haystack,
-    "version",
-    OLD_VERSIONS
-)
+def filepath_to_url(haystack):
+    domain = "https://haystack.deepset.ai"
+    ret = []
+    for haystack_filepath, haystack_filename, haystack_versions, haystack_types in haystack:
+        if haystack_versions == "latest" and haystack_types == "docs":
+            haystack_url = haystack_filepath.replace("docs/latest", domain)
+            sections = haystack_url.split("/")
+            new_sections = sections[:-1]
+            new_sections.append(sections[-1].replace(".mdx", "").replace("_", "-"))
+            haystack_url = "/".join(new_sections)
+        elif haystack_versions != "latest" and haystack_types == "docs":
+            sections = haystack_filepath.split("/")
+            new_sections = [sections[2], sections[1]]
+            new_sections.append(sections[-1].replace(".mdx", "").replace("_", "-"))
+            new_sections = [domain] + new_sections
+            haystack_url = "/".join(new_sections)
+        elif haystack_versions == "latest" and haystack_types == "reference":
+            sections = haystack_filepath.split("/")
+            new_sections = [domain, "reference"]
+            new_sections.append(sections[-1].replace(".md", "").replace("_", "-"))
+            haystack_url = "/".join(new_sections)
+        elif haystack_versions != "latest" and haystack_types == "reference":
+            sections = haystack_filepath.split("/")
+            new_sections = [domain, "reference", sections[3]]
+            new_sections.append(sections[-1].replace(".md", "").replace("_", "-"))
+            haystack_url = "/".join(new_sections)
+        else:
+            raise Exception
+        ret.append((haystack_url, haystack_filename, haystack_versions, haystack_types))
+    return ret
 
+if __name__ == "__main__":
 
+    readme_files = get_readme_files()
+    readme_filenames = [x.split("/")[-1] for x in readme_files]
+    readme_files_versions = [extract_version(x) for x in readme_files]
+    readme_files_type = [extract_type(x) for x in readme_files]
+    readme = zip(readme_files, readme_filenames, readme_files_versions, readme_files_type)
+    readme = update_readme_filenames(readme)
+    df_readme = pd.DataFrame.from_records(readme, columns=["filepath", "filename", "version", "type"])
+    df_readme = filter_rows_by_values(
+        df_readme,
+        "version",
+        OLD_VERSIONS
+    )
+    print()
 
+    haystack_docs = get_all_files("docs")
+    haystack_docs_type = ["docs"] * len(haystack_docs)
+    haystack_api = get_all_api_files("../haystack/docs")
+    haystack_api_type = ["reference"] * len(haystack_api)
+    haystack_files = haystack_docs + haystack_api
+    haystack_filepath = [x[2] for x in haystack_files]
+    haystack_filename = [x[1] for x in haystack_files]
+    haystack_types = haystack_docs_type + haystack_api_type
+    haystack_versions = [extract_version(x[0]) for x in haystack_files]
+    haystack = zip(haystack_filepath, haystack_filename, haystack_versions, haystack_types)
+    haystack = filepath_to_url(haystack)
+    df_haystack = pd.DataFrame.from_records(haystack, columns=["filepath", "filename", "version", "type"])
+    df_haystack = filter_rows_by_values(
+        df_haystack,
+        "version",
+        OLD_VERSIONS
+    )
 
+    df_filled = pd.read_csv("df_haystack.csv")
+    matches = []
+    for i, row in df_haystack.iterrows():
+        potential_match = match(row, df_readme)
+        if potential_match is None:
+            potential_match_df = df_filled[
+                (df_filled["filename"] == row["filename"]) &
+                (df_filled["version"] == row["version"]) &
+                (df_filled["type"] == row["type"])
+            ]
+            if len(potential_match_df) == 1:
+                potential_match = potential_match_df["filepath"].values[0]
+        matches.append(potential_match)
+    df_haystack["readme_filepath"] = matches
+    df_missing = df_haystack[df_haystack["readme_filepath"].isnull()]
+    print(matches.count(None))
+    print()
 
-matches = []
-for i, row in df_haystack.iterrows():
-    matches.append(match(row, df_readme))
-df_haystack["readme_filepath"] = matches
-df_missing = df_haystack[df_haystack["readme_filepath"].isnull()]
-print(matches.count(None))
-print()
-
-# Check overview and preprocessing
-
-
-
-
-
-# DEAL WITH OVERVIEW / PREPROCESSING PAGES WITH SAME NAME
