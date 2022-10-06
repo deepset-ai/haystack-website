73eedf641a2a8b4114daad5d2e5d397ee4d97582
Tuana Celik <tuana.celik@deepset.ai>
2022-10-06 11:44:18 +0200

    all subsections that move to new docs excluded from sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,10 +1,10 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://haystack.deepset.ai',
     generateRobotsTxt: true, 
-    exclude: ['/tutorials/v*','/overview/v*', '/components/v*', '/usage/*', '/reference/v*', '/guides/v*', '/pipeline_nodes/v*', '/benchmarks/v*'],
+    exclude: ['/tutorials/v*','/overview/v*', '/overview/glossary', '/overview/faq', '/overview/migration', '/overview/installation', '/overview/telemetry', '/components/*', '/usage/*', '/reference/*', '/guides/*', '/pipeline_nodes/*', '/benchmarks/v*', '/docs/'],
     robotsTxtOptions: {
       policies: [
-                 { userAgent: "*", disallow:['/tutorials/v*','/overview/v*', '/components/v*', '/usage/*', '/reference/v*', '/guides/v*', '/pipeline_nodes/v*', '/benchmarks/v*']},
+                 { userAgent: "*", disallow:['/tutorials/v*','/overview/v*', '/overview/glossary', '/overview/faq', '/overview/migration', '/overview/installation', '/overview/telemetry', '/components/*', '/usage/*', '/reference/*', '/guides/*', '/pipeline_nodes/*', '/benchmarks/v*', '/docs/']},
         ]
   }
-  }
+}
